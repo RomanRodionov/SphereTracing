c5926f1682b47eb8591b31b04f7994ca88feeac8
Sammael <Sammael>
2024-01-16 20:51:03 +0300

    code template and references

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+
+#include "public_camera.h"
+#include "public_scene.h"
+#include "public_image.h"
+
+int main()
+{
+  return 0;
+}
